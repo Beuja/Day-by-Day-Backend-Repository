20ffdff5cfe610290312ade345741f8fad04d781
Wook <ohkingsman@naver.com>
2026-05-24 17:42:13 +0900

    books swagger 반영

--- daybydaybackend/books/urls.py
+++ daybydaybackend/books/urls.py
@@ -5,5 +5,5 @@ app_name = 'books'
 
 urlpatterns = [
     # 감정 기반 도서 추천 API (뷰 함수 시그니처 및 POST 요청 형식에 최적화)
-    path('recommend/books/', views.recommend_books_views, name='recommend_books')
+    path('recommend/books/<int:diary_id>/', views.recommend_books_views, name='recommend_books')
 ]
