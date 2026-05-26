74f226a3bd6d4d2fc703748d03f8501bb672ad32
unknown <seolsoyeon12@gmail.com>
2026-05-26 17:17:03 +0900

    pull main

--- daybydaybackend/music_movie/serializers.py
+++ daybydaybackend/music_movie/serializers.py
@@ -1,3 +1,4 @@
+# music_movie/serializers.py
 from rest_framework import serializers
 
 class ContentRecommendationRequestSerializer(serializers.Serializer):
@@ -14,15 +15,57 @@ class ContentRecommendationRequestSerializer(serializers.Serializer):
     count = serializers.IntegerField(default=3, min_value=1, max_value=20, required=False)
 
 class MusicResponseSerializer(serializers.Serializer):
-    track_id = serializers.IntegerField()
+    track_id = serializers.SerializerMethodField()
     title = serializers.CharField()
-    artist = serializers.CharField(allow_blank=True, required=False, default='')
-    image_url = serializers.URLField(allow_blank=True, required=False, default='')
-    tags = serializers.ListField(child=serializers.CharField(), default=list)
+    artist = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
+
+    def get_track_id(self, obj):
+        if hasattr(obj, 'id'): return obj.id
+        return obj.get('track_id') if isinstance(obj, dict) else None
+
+    def get_artist(self, obj):
+        if hasattr(obj, 'artist'): return obj.artist if obj.artist else ''
+        return obj.get('artist', '') if isinstance(obj, dict) else ''
+
+    def get_image_url(self, obj):
+        if hasattr(obj, 'image_url'): return obj.image_url if obj.image_url else ''
+        return obj.get('image_url', '') if isinstance(obj, dict) else ''
+
+    def get_tags(self, obj):
+        if hasattr(obj, 'tags'): return obj.tags if isinstance(obj.tags, list) else []
+        return obj.get('tags', []) if isinstance(obj, dict) else []
+
+    def get_score(self, obj):
+        if hasattr(obj, 'score'): return obj.score
+        return obj.get('score', 0.0) if isinstance(obj, dict) else 0.0
 
 class MovieResponseSerializer(serializers.Serializer):
-    movie_id = serializers.IntegerField()
+    movie_id = serializers.SerializerMethodField()
     title = serializers.CharField()
-    director = serializers.CharField(allow_blank=True, required=False, default='')
-    image_url = serializers.URLField(allow_blank=True, required=False, default='')
-    tags = serializers.ListField(child=serializers.CharField(), default=list)
+    director = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
+
+    def get_movie_id(self, obj):
+        if hasattr(obj, 'tmdb_id'): return obj.tmdb_id
+        return obj.get('movie_id') if isinstance(obj, dict) else None
+
+    def get_director(self, obj):
+        if hasattr(obj, 'director'): return obj.director if obj.director else ''
+        return obj.get('director', '') if isinstance(obj, dict) else ''
+
+    def get_image_url(self, obj):
+        if hasattr(obj, 'poster_path'): return obj.poster_path if obj.poster_path else ''
+        return obj.get('image_url', '') if isinstance(obj, dict) else ''
+
+    def get_tags(self, obj):
+        if hasattr(obj, 'genre'): return [obj.genre] if obj.genre else []
+        return obj.get('tags', []) if isinstance(obj, dict) else []
+
+    def get_score(self, obj):
+        if hasattr(obj, 'score'): return obj.score
+        return obj.get('score', 0.0) if isinstance(obj, dict) else 0.0
